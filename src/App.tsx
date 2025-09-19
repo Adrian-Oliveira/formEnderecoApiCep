825eaef18ee275911d64323e0b08d1b70892da7e
adrian <adrian.oliveira.dev@gmail.com>
2025-09-19 19:54:27 -0300

    fetching api

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [cep, setCep] = useState<string>()
+
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try{
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        const result = await response.json();
+        console.log(result)
+        
+      }
+      catch(e){
+        console.log(e)
+      }
+    }
+    fetchData();
+
+  }, [cep])
+
+  const handleCepChange = (e)=>{
+    const value = e.target.value
+    console.log(e.target.value)
+    if (value.length === 8){
+      setCep(value)
+    }
+  } 
 
   return (
     <>
       <form action="" className="endereco">
-        <input type="text" className="inputText" id="cep" />
+        <input 
+          type="text" 
+          className="inputText" 
+          id="cep" 
+          onChange={handleCepChange}
+          placeholder="cep"
+          maxLength={8}/>
         <input type="text" className="inputText" id="pais"/>
         <input type="text" className="inputText" id="estado"/>
         <input type="text" className="inputText" id="cidade"/>
